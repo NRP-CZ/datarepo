ffc4c23ca96e621be9f239c6e5a9e9d29fd32274
Mirek Simek <miroslav.simek@gmail.com>
2025-10-09 10:57:06 +0200

    release script

---
+++ scripts/release-14.0.0a1.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# This script prepares a new alpha release.
+#
+
+set -exo pipefail
+
+cd "$(dirname "$0")/.."
+
+if [ -d .venv ]; then
+    source .venv/bin/activate
+fi
+
+if [ -z "$BUCKET_NAME" ] ; then
+    echo "BUCKET_NAME is not set"
+    exit 1
+fi
+
+if [ -z "$USER_PASSWORD" ]; then
+  echo "Error: USER_PASSWORD environment variable is not set."
+  exit 1
+fi
+
+./run.sh cli services setup
+
+invenio files location create --default S3 s3://${BUCKET_NAME};
+
+
+invenio users create -a -c --password "$USER_PASSWORD" admin@test.com
+invenio users create -a -c --password "$USER_PASSWORD" user@test.com
+
+
+invenio roles create administration
+invenio access allow administration-access role administration
+invenio access allow administration-moderation role administration
+
+invenio roles add admin@test.com administration
