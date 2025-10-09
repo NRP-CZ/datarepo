51cc80c8902e53c1a13d4c2aec0bc4cf8b2f2adb
Mirek Simek <miroslav.simek@gmail.com>
2025-10-09 07:56:04 +0200

    Initial RDM-14 commit

--- ui/datasets/semantic-ui/js/datasets/forms/FormFieldsContainer.jsx
+++ ui/datasets/semantic-ui/js/datasets/forms/FormFieldsContainer.jsx
@@ -1,48 +1,59 @@
 import * as React from "react";
 import {
-    useFormConfig,
-    FormikStateLogger,
-    FilesField,
-    TextField,
+  useFormConfig,
+  FormikStateLogger,
+  TextField,
 } from "@js/oarepo_ui/forms";
-import { CommunitySelector } from "@js/communities_components/CommunitySelector/CommunitySelector";
-import { LocalVocabularySelectField } from "@js/oarepo_vocabularies";
 import { AccordionField } from "react-invenio-forms";
 import { i18next } from "@translations/i18next";
+import { UppyUploader } from "@js/invenio_rdm_records";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
-const FormFieldsContainer = () => {
-    const { formConfig, files: recordFiles } = useFormConfig();
-
-    return (
-        <React.Fragment>
-            <CommunitySelector />
-            <AccordionField
-                includesPaths={["metadata.title", "metadata.languages"]}
-                active
-                label={i18next.t("Basic information")}
-            >
-                <TextField fieldPath="metadata.title" />
-                <LocalVocabularySelectField
-                    fieldPath="metadata.languages"
-                    optionsListName="languages"
-                />
-            </AccordionField>
-            <AccordionField
-                includesPaths={["files.enabled"]}
-                active
-                label={
-                    <label htmlFor="files.enabled">{i18next.t("Files upload")}</label>
-                }
-                data-testid="filesupload-button"
-            >
-                <FilesField
-                    recordFiles={recordFiles}
-                    allowedFileTypes={formConfig.allowed_file_extensions}
-                />
-            </AccordionField>
-            {process.env.NODE_ENV === "development" && <FormikStateLogger />}
-        </React.Fragment>
-    );
+const FormFieldsContainerComponent = ({ record }) => {
+  const formConfig = useFormConfig();
+  const { filesLocked } = formConfig;
+  return (
+    <React.Fragment>
+      <AccordionField
+        includesPaths={["metadata.title"]}
+        active
+        label={i18next.t("Basic information")}
+      >
+        <TextField fieldPath="metadata.title" />
+      </AccordionField>
+      <AccordionField
+        includesPaths={["files.enabled"]}
+        active
+        label={
+          <label htmlFor="files.enabled">{i18next.t("Files upload")}</label>
+        }
+        data-testid="filesupload-button"
+      >
+        <UppyUploader
+          isDraftRecord={!record.is_published}
+          config={formConfig}
+          quota={formConfig.quota}
+          decimalSizeDisplay={formConfig.decimal_size_display}
+          allowEmptyFiles={formConfig.allow_empty_files}
+          fileUploadConcurrency={formConfig.file_upload_concurrency}
+          showMetadataOnlyToggle={false}
+          filesLocked={filesLocked}
+        />
+      </AccordionField>
+      {process.env.NODE_ENV === "development" && <FormikStateLogger />}
+    </React.Fragment>
+  );
 };
 
-export default FormFieldsContainer;
+FormFieldsContainerComponent.propTypes = {
+  record: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    record: state.deposit.record,
+  };
+};
+
+export default connect(mapStateToProps)(FormFieldsContainerComponent);
