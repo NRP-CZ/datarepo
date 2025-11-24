ce23dd677f24f2c6e20cb8d8d5d09effb46b90c3
Mirek Simek <3385642+mesemus@users.noreply.github.com>
2025-11-24 15:53:04 +0100

    Python 14 and new model configuration

--- datasets/model.py
+++ datasets/model.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from ccmm_invenio.models import ccmm_production_preset_1_1_0
 from invenio_records_permissions.generators import AuthenticatedUser
 from oarepo_model.api import model
-from oarepo_model.customizations import AddMixins
+from oarepo_model.customizations import PrependMixin
 from oarepo_model.model import ModelMixin
 
 
@@ -21,7 +21,7 @@ datasets_model = model(
     metadata_type="CCMMProductionDataset",
     customizations=[
         # TODO: remove this customization if you use oarepo-communities for RDM 14
-        AddMixins("PermissionPolicy", DatasetsPermissionPolicyMixin),
+        PrependMixin("PermissionPolicy", DatasetsPermissionPolicyMixin),
     ],
     configuration={"ui_blueprint_name": "datasets_ui"},
 )
