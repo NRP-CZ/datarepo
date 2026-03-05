a84a353a4c13a24ed839a3c46668654dc4958701
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-03-05 12:16:10 +0100

    removed formfieldscontainer for monolith

--- ui/datasets/semantic-ui/js/datasets/forms/index.js
+++ ui/datasets/semantic-ui/js/datasets/forms/index.js
@@ -7,7 +7,6 @@ import {
   CCMMDepositRecordSerializer,
   CCMMSections,
 } from "@js/ccmm_invenio/forms";
-import FormFieldsContainer from "./FormFieldsContainer";
 
 const { rootEl, config, ...rest } = parseFormAppConfig();
 const recordSerializer = new CCMMDepositRecordSerializer(
@@ -19,12 +18,9 @@ const parametrizeEDTFSingleDatePicker = parametrize(EDTFSingleDatePicker, {
   customInputProps: { width: 16 },
 });
 
-const overridableIdPrefix = config.overridableIdPrefix;
-
 export const componentOverrides = {
   "InvenioRdmRecords.DepositForm.DatesField.DateField":
     parametrizeEDTFSingleDatePicker,
-  [`${overridableIdPrefix}.FormFields.container`]: FormFieldsContainer,
 };
 
 ReactDOM.render(
