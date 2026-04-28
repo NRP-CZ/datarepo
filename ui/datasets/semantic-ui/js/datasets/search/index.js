beb49ae1f41745bd720ff670412df5d80152e8da
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-04-28 10:40:25 +0200

    fix: search bar lined up with rest of content

--- ui/datasets/semantic-ui/js/datasets/search/index.js
+++ ui/datasets/semantic-ui/js/datasets/search/index.js
@@ -3,7 +3,10 @@ import {
   createSearchAppsInit,
 } from "@js/oarepo_ui/search";
 import ResultsListItem from "./ResultsListItem";
-
+import { Grid } from "semantic-ui-react";
+import React from "react";
+import { SearchBar } from "@js/invenio_search_ui/components";
+import { buildUID } from "react-searchkit";
 /** NOTE: This reads configs for any search app present on a page
  *   In HTML/Jinja, a single search app instance is typically represented
 @@ -14,13 +15,13 @@ import {
@@ -12,11 +15,22 @@ import ResultsListItem from "./ResultsListItem";
  */
 const [{ overridableIdPrefix }] = parseSearchAppConfigs();
 
+const SearchBarContainer = () => (
+  <Grid relaxed padded>
+    <Grid.Row>
+      <Grid.Column width={16}>
+        <SearchBar buildUID={buildUID} appName={overridableIdPrefix} />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+);
+
 export const componentOverrides = {
   /** NOTE: Then you can then replace any existing search ui
    * component with your own implementation, e.g.:
    */
   [`${overridableIdPrefix}.ResultsList.item`]: ResultsListItem,
+  [`${overridableIdPrefix}.SearchApp.searchbarContainer`]: SearchBarContainer,
 };
 
 createSearchAppsInit({ componentOverrides });
