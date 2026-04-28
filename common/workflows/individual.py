c0e43c1122886e5dffd69b82a1862af4cb063757
Dusan Stojanovic <dusanstojanovic256@gmail.com>
2026-04-28 10:38:01 +0200

    uv lock

--- common/workflows/individual.py
+++ common/workflows/individual.py
@@ -1,4 +1,3 @@
-from invenio_rdm_records.services.generators import RecordOwners
 from invenio_records_permissions.generators import AuthenticatedUser
 from oarepo_workflows.services.permissions import DefaultWorkflowPermissions
 
@@ -6,4 +5,4 @@ from oarepo_workflows.services.permissions import DefaultWorkflowPermissions
 class IndividualDepositionWorkflowPermissions(DefaultWorkflowPermissions):
     can_create = [AuthenticatedUser()]
     can_review = [AuthenticatedUser()]  # for link creation
-    can_manage = [RecordOwners()]
+    can_manage = [AuthenticatedUser()]
