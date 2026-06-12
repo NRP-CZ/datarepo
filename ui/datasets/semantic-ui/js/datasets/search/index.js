094a0f05efa3bf6756240a4854b28537e1aa3bac
Ducica <34883098+Ducica@users.noreply.github.com>
2026-06-12 18:12:00 +0200

    aligned styles with design (#56)
    * aligned styles with design
    
    * fixed code smells
    
    * added translations
    
    * facet translations
    
    * moved components to oarepo-ui
    
    * added draft tag
    
    * minor fixes

--- ui/datasets/semantic-ui/js/datasets/search/index.js
+++ ui/datasets/semantic-ui/js/datasets/search/index.js
@@ -1,36 +1,29 @@
 import {
   parseSearchAppConfigs,
   createSearchAppsInit,
+  SearchAppFacets,
+  SearchAppLayout,
 } from "@js/oarepo_ui/search";
 import ResultsListItem from "./ResultsListItem";
-import { Grid } from "semantic-ui-react";
-import React from "react";
-import { SearchBar } from "@js/invenio_search_ui/components";
-import { buildUID } from "react-searchkit";
-/** NOTE: This reads configs for any search app present on a page
- *   In HTML/Jinja, a single search app instance is typically represented
-@@ -14,13 +15,13 @@ import {
-/** NOTE: To customize components in a specific search app instance,
- *   you need to obtain its `overridableIdPrefix` from the corresponding config first
- */
+import { parametrize } from "react-overridable";
+import { i18next } from "@translations/i18next";
+
 const [{ overridableIdPrefix }] = parseSearchAppConfigs();
 
-const SearchBarContainer = () => (
-  <Grid relaxed padded>
-    <Grid.Row>
-      <Grid.Column width={16}>
-        <SearchBar buildUID={buildUID} appName={overridableIdPrefix} />
-      </Grid.Column>
-    </Grid.Row>
-  </Grid>
-);
+const SearchAppFacetsWithTitle = parametrize(SearchAppFacets, {
+  title: i18next.t("Data Catch-all Repository"),
+});
+
+const SearchAppLayoutWithTip = parametrize(SearchAppLayout, {
+  searchBarTip: i18next.t(
+    "TIP: Most of the content is in English. You will get more results by using English terms.",
+  ),
+});
 
 export const componentOverrides = {
-  /** NOTE: Then you can then replace any existing search ui
-   * component with your own implementation, e.g.:
-   */
   [`${overridableIdPrefix}.ResultsList.item`]: ResultsListItem,
-  [`${overridableIdPrefix}.SearchApp.searchbarContainer`]: SearchBarContainer,
+  [`${overridableIdPrefix}.SearchApp.facets`]: SearchAppFacetsWithTitle,
+  [`${overridableIdPrefix}.SearchApp.layout`]: SearchAppLayoutWithTip,
 };
 
 createSearchAppsInit({ componentOverrides });
