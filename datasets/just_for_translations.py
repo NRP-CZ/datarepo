ae723c3a97ea72a96d6437833a1d7c1ed12519c7
Alzbeta Pokorna <lili3.pokorna@seznam.cz>
2026-06-18 09:52:09 +0200

    fix: subjects vocabulary facet translation

--- datasets/just_for_translations.py
+++ datasets/just_for_translations.py
@@ -3,3 +3,4 @@ from invenio_i18n import lazy_gettext as _
 # TODO: this should be handled in a better way
 _("metadata/languages.label")
 _("metadata/creators/person_or_org/name.label")
+_("metadata/subjects/subject.label")
