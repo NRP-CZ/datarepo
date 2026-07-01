5ee6b2da97c77024239aac73c56fc4eeb2b63487
Ducica <34883098+Ducica@users.noreply.github.com>
2026-07-01 10:12:05 +0200

    fix: taking to repo instructions page in case no right to deposit (#105)

--- ui/datasets/__init__.py
+++ ui/datasets/__init__.py
@@ -58,6 +58,12 @@ def init_menu(app):
             order=1,
             visible_when=can_view_deposit_page,
         )
+        current_menu.submenu("plus.create_datasets_docs").register(
+            external_url="https://docs.nrp.eosc.cz/en/docs/end_users/catch-all-data-repository/catch-all-getting-started#registering-as-a-depositor-for-the-repository",
+            text=_("New Dataset"),
+            order=1,
+            visible_when=lambda: not can_view_deposit_page(),
+        )
         current_menu.submenu("main.about_repository").register(
             external_url=app.config.get("ABOUT_REPOSITORY_DOCS_URL"),
             text=_("About repository"),
