b2a2a4f060ca8cfef67a3c36c840229d24058e20
Miroslav Bauer <mirekys@gmail.com>
2026-09-03 17:36:26 +0200

    feat(upgrade_to_1.1.0): auto-create submitters role

--- scripts/upgrade_to_1_1_0.sh
+++ scripts/upgrade_to_1_1_0.sh
@@ -16,5 +16,7 @@ invenio rdm-records custom-fields init
 invenio communities custom-fields init
 invenio queues declare
 
+invenio roles create submitters
+
 # re-index the records
 invenio rdm rebuild-all-indices
